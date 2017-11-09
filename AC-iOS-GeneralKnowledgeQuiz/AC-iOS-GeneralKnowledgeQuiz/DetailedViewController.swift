bb9b16348822493c51e880da987094706857d2ca
Ashlee Krammer <ashlee.alyse@me.com>
2017-11-09 16:00:07 -0500

    Step 2

---
+++ AC-iOS-GeneralKnowledgeQuiz/AC-iOS-GeneralKnowledgeQuiz/DetailedViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DetailedViewController.swift
+//  AC-iOS-GeneralKnowledgeQuiz
+//
+//  Created by Ashlee Krammer on 11/9/17.
+//  Copyright © 2017 AC-iOS. All rights reserved.
+//
+
+import UIKit
+
+class DetailedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
--- AC-iOS-GeneralKnowledgeQuiz/AC-iOS-GeneralKnowledgeQuiz/ViewController.swift
+++ AC-iOS-GeneralKnowledgeQuiz/AC-iOS-GeneralKnowledgeQuiz/ViewController.swift
@@ -8,21 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    //Outlets
+    @IBOutlet weak var tableView: UITableView!
+    
+    //Variables
+    var questions = Question.questions
+    
+    //Override ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+    }
+    
+    //Two Required Functions for Data Source
+
+    //Number of rows
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return questions.count
+    }
+    
+    //Creating cell here
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let aQuestion = questions[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "questionCell", for: indexPath)
+        cell.detailTextLabel?.text = "Category: \(aQuestion.category), Difficulty: \(aQuestion.difficulty), Type: \(aQuestion.type)"
+        cell.textLabel?.text = aQuestion.question
         
-        
-        
+        return cell
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //Segue
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destination = segue.destination as? DetailedViewController {
+            let selectedRow = tableView.indexPathForSelectedRow?.row
+            let selectedQuestion = questions[selectedRow!]
+            destination. = selctedQuestion 
+        }
     }
-
-
 }
 
